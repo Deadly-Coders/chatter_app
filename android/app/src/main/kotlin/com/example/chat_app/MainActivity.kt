405fad8da5bd225b2437944732db2b49b2b99030
FlutterBasedApp <vishwajeetsaware@gmail.com>
2024-09-21 04:33:26 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package your.package.name
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+
---
+++ android/app/src/main/kotlin/com/ishandeveloper/chatter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ishandeveloper.chatter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
